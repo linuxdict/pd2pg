132469792a0360224c442808d96a23ad898941d7
Linuxdict <edy@linuxdict.com>
2019-08-10 09:02:30 +0800

    Update individual-detail.sql
    replace with Name

--- examples/individual-detail.sql
+++ examples/individual-detail.sql
@@ -1,9 +1,9 @@
 -- Show details about an individual's notifications over the past week.
--- Note that the user's email is in the query.
+-- Note that the user name is in the query.
 
 select
   incidents.html_url as incident_url,
-  log_entries.created_at at time zone 'America/Los_Angeles' as notification_time,
+  log_entries.created_at at time zone 'Asia/Shanghai' as notification_time,
   log_entries.notification_type as notification_type,
   incidents.trigger_summary_subject,
   services.name as service_name
@@ -13,9 +13,10 @@ from
   incidents,
   services
 where
-  users.email = 'mark@stripe.com' and
+-- or users.email = 'user@company.com' and 
+  users.name = 'username' and
   log_entries.user_id = users.id and
-  log_entries.type = 'notify' and
+-- Seems not filled anymore, comment log_entries.type = 'notify' and
   log_entries.created_at > now() - '1 week'::interval and
   incidents.id = log_entries.incident_id and
   incidents.service_id = services.id
